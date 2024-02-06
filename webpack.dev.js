9e23eda62d2ff9b92cec5d6ed6cfc6727ad9bf8d
Mehmet Eren Kala <43720526+Mekala02@users.noreply.github.com>
2024-02-06 16:52:25 +0300

    seperate dev and prod

---
+++ webpack.dev.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports  = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    },
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist")
+        },
+        port: 5500,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Webpack App',
+          filename: 'index.html',
+          template: 'src/index.html',
+        }),
+        // new BundleAnalyzerPlugin(),
+      ],
+    resolve: {
+    extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports  = {
+    mode: "production", // Change mode to production
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "production"),
+      filename: "[name].[contenthash].js", // Include contenthash in filename
+      clean: true,
+    },
+    devtool: false, // Remove source maps
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack App',
+        filename: 'index.html',
+        template: 'src/index.html',
+      }),
+    ],
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "images",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin(), // Minify JavaScript
+      ],
+    },
+  };
